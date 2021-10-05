ec9068c4089e4ff94d3752cd039b9aab08b3b016
Labocania <hugokl@outlook.com>
2021-10-05 18:13:30 -0300

    Initial Entity Framework Core and Postgresql configuration.

--- NineNineQuotes/Startup.cs
+++ NineNineQuotes/Startup.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -16,17 +17,19 @@ namespace NineNineQuotes
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
         {
             Configuration = configuration;
+            Environment = env;
         }
 
         public IConfiguration Configuration { get; }
+        public IWebHostEnvironment Environment { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddDbProvider(Environment, Configuration);
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
@@ -54,6 +57,42 @@ namespace NineNineQuotes
             {
                 endpoints.MapControllers();
             });
+        }
+    }
+
+    public static class AddDbProviderExtensions
+    {
+        public static IServiceCollection AddDbProvider(this IServiceCollection services, IWebHostEnvironment env, IConfiguration config)
+        {
+            string connStr = "";
+            if (env.EnvironmentName == "Development")
+            {
+                connStr = config.GetConnectionString("DefaultConnection");
+            }
+
+            if (env.EnvironmentName == "Production")
+            {
+                string connUrl = config.GetSection("DATABASE_URL").Value;
+
+                // Parse connection URL to connection string for Npgsql
+                connUrl = connUrl.Replace("postgres://", string.Empty);
+
+                string pgUserPass = connUrl.Split("@")[0];
+                string pgHostPortDb = connUrl.Split("@")[1];
+                string pgHostPort = pgHostPortDb.Split("/")[0];
+
+                string pgDb = pgHostPortDb.Split("/")[1];
+                string pgUser = pgUserPass.Split(":")[0];
+                string pgPass = pgUserPass.Split(":")[1];
+                string pgHost = pgHostPort.Split(":")[0];
+                string pgPort = pgHostPort.Split(":")[1];
+
+                connStr = $"Server={pgHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb};sslmode=Prefer;Trust Server Certificate=true";
+            }
+
+            services.AddDbContext<Data.AppDbContext>(options => options.UseNpgsql(connStr,
+                o => o.UseQuerySplittingBehavior(QuerySplittingBehavior.SplitQuery)));
+            return services;
         }
     }
 }
